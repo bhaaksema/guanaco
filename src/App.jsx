70085bf14a75f2209b99962e53cd846302902e39
bhaaksema <17254355+bhaaksema@users.noreply.github.com>
2023-06-17 16:02:50 +0000

    Visual update

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,59 @@
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 import Proof from "./components/Proof";
 
 export default function App() {
-  const A2 = "(K{a} f1 & K{a} (f1 -> f2)) -> K{a} f2";
-  const A2p = "K{a} (f1 -> f2) -> (K{a} f1 -> K{a} f2)";
+  const maintainer = "https://github.com/bhaaksema";
+  const documentation = "https://bhaaksema.github.io/guanaco-doc/";
 
   return (
-    <Container className="d-flex justify-content-center">
-      <Card className="mt-5">
-        <Card.Header>
-          <Card.Title>
-            Guanaco - A Syntactic Proof Guide for Epistemic Logic
-          </Card.Title>
-          <Card.Text>
-            This tool helps people to determine which rules can be applied and
-            to decide whether these rules have been applied correctly.
-            <br /> <br />
-            Examples:
-          </Card.Text>
-          <ul>
-            <li>
-              <code>{A2}</code>
-            </li>
-            <li>
-              <code>{A2p}</code>
-            </li>
-          </ul>
-        </Card.Header>
-        <Card.Body>
-          <Proof />
-        </Card.Body>
-      </Card>
-    </Container>
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand>Guanaco</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href={documentation} target="_blank">
+              Documentation
+            </Nav.Link>
+          </Nav>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Maintained by:{" "}
+              <a href={maintainer} target="_blank" rel="noreferrer">
+                bhaaksema
+              </a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container className="d-flex justify-content-center">
+        <Card border="dark" className="mt-4">
+          <Card.Header>
+            <Card.Title as="h3">
+              A Syntactic Proof Guide for Epistemic Logic
+            </Card.Title>
+            <Card.Text>
+              The strategy that Guanaco employs is building proofs bottom-up.
+              This means that users start with the formula that they wish to
+              derive. If this formula can only be derived by a rule, then
+              Guanaco exploits the properties of this formula to extract new
+              formulas the relevant rule is applied to. If the formula is an
+              instantiation of an axiom, no new formulas are extracted. The same
+              process is then applied until each formula has a justification and
+              no new ones are produced. At that point, the proof is complete.
+              Currently, axiom A1 for propositional tautologies is not yet
+              validated automatically.
+            </Card.Text>
+          </Card.Header>
+          <Card.Body>
+            <Proof />
+          </Card.Body>
+        </Card>
+      </Container>
+    </>
   );
 }
